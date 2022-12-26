e6116496014d337323971451189663e662035089
thanhduycao <thanhduycao1202@gmail.com>
2022-12-26 20:25:30 +0700

    feat: construct event detail screen baseline

---
+++ components/screens/EventDetailScreen/EventInfoTab/EventInfoTab.js
@@ -0,0 +1,14 @@
+import { View, Text, ScrollView } from "react-native";
+
+
+export default function EventInfoTab() {
+    return (
+        <ScrollView
+            style={{ paddingTop: 60 }}
+        >
+            <Text style={{
+                color: "#FFFFFF",
+            }}>Event Info</Text>
+        </ScrollView>
+    );
+}
---
+++ components/screens/EventDetailScreen/EventReviewsTab/EventReviewsTab.js
@@ -0,0 +1,13 @@
+import { View, Text, ScrollView } from "react-native";
+
+export default function EventReviewsTab() {
+    return (
+        <ScrollView
+            style={{ paddingTop: 60 }}
+        >
+            <Text style={{
+                color: "#FFFFFF",
+            }}>Event Reviews</Text>
+        </ScrollView>
+    );
+}
