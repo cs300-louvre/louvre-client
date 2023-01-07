9c20073e0639da176326b625e1e68c63deb13df9
Phu Nguyen <npphu20@apcs.fitus.edu.vn>
2023-01-07 18:01:57 +0700

    feat: user page

--- components/screens/AccountScreen/FollowingTab/FollowingTab.tsx
+++ components/screens/AccountScreen/FollowingTab/FollowingTab.tsx
@@ -3,15 +3,17 @@ import EventCard from "../../../elements/EventCard/EventCard";
 import MuseumCard from "../../../elements/MuseumCard/MuseumCard";
 import { useNavigation } from "@react-navigation/native";
 import { fakeEventResponse, fakeMuseumResponse } from "../../../../mock";
-
-const results = Array.from(Array(10), () => {
-  const random = Math.round(Math.random());
-  if (random) return fakeEventResponse();
-  return fakeMuseumResponse();
-});
+import useGetFollowedMuseum from "../../../../hooks/me/useGetFollowedMuseums";
+import useGetFollowedEvent from "../../../../hooks/me/useGetFollowedEvents";
+import { shuffleArray } from "../../../../utils";
 
 export const FollowingTab = () => {
   const navigation = useNavigation<any>();
+  const { data: museums } = useGetFollowedMuseum();
+  const { data: events } = useGetFollowedEvent();
+  const results =
+    museums && events ? shuffleArray([].concat(museums, events)) : null;
+
   return (
     <ScrollView
       style={{
@@ -20,37 +22,38 @@ export const FollowingTab = () => {
       }}
       contentContainerStyle={{ paddingBottom: 50, paddingTop: 10 }}
     >
-      {results.map((result: any) =>
-        result?.eventId ? (
-          <EventCard
-            key={result.eventId}
-            item={result}
-            handlePress={() =>
-              navigation.navigate("Account", {
-                screen: "EventDetail",
-                params: {
-                  eventId: result.evenId,
-                  navigationRoot: "Account",
-                },
-              })
-            }
-          />
-        ) : (
-          <MuseumCard
-            key={result.museumId}
-            item={result}
-            handlePress={() =>
-              navigation.navigate("Account", {
-                screen: "MuseumDetail",
-                params: {
-                  museumId: result.museumId,
-                  navigationRoot: "Account",
-                },
-              })
-            }
-          />
-        )
-      )}
+      {results &&
+        results.map((result: any) =>
+          result?.eventId ? (
+            <EventCard
+              key={result.eventId}
+              item={result}
+              handlePress={() =>
+                navigation.navigate("Account", {
+                  screen: "EventDetail",
+                  params: {
+                    eventId: result.evenId,
+                    navigationRoot: "Account",
+                  },
+                })
+              }
+            />
+          ) : (
+            <MuseumCard
+              key={result.museumId}
+              item={result}
+              handlePress={() =>
+                navigation.navigate("Account", {
+                  screen: "MuseumDetail",
+                  params: {
+                    museumId: result.museumId,
+                    navigationRoot: "Account",
+                  },
+                })
+              }
+            />
+          )
+        )}
     </ScrollView>
   );
 };
--- components/screens/AccountScreen/RatingTab/RatingTab.tsx
+++ components/screens/AccountScreen/RatingTab/RatingTab.tsx
@@ -1,12 +1,10 @@
 import { fakeRating } from "../../../../mock";
 import { ScrollView, View } from "react-native";
 import Rating from "../../../elements/Rating/Rating";
-
-const ratings = Array.from(Array(10), () => {
-  return fakeRating();
-});
+import useGetMyRatings from "../../../../hooks/me/useGetMyRatings";
 
 export const RatingTab = () => {
+  const { data: ratings } = useGetMyRatings();
   return (
     <View>
       <ScrollView
@@ -16,9 +14,10 @@ export const RatingTab = () => {
         }}
         contentContainerStyle={{ paddingBottom: 80, paddingTop: 10 }}
       >
-        {ratings.map((rating) => (
-          <Rating item={rating} key={rating.ratingId} />
-        ))}
+        {ratings &&
+          ratings.map((rating) => (
+            <Rating item={rating} key={rating.ratingId} />
+          ))}
       </ScrollView>
     </View>
   );
